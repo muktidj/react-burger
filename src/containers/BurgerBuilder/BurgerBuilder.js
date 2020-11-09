5fa5aaf3ab3a0f2df5145a75c5366f39a1f55b7d
muktidj <muktidwijatmoko@gmail.com>
2020-11-09 21:12:41 +0700

    bab 8, 136 Adding the Orders Button

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -20,7 +20,20 @@ class BurgerBuilder extends Component {
             meat: 0
 
         },
-        totalPrice : 4
+        totalPrice : 4,
+        purchasable: false
+    }
+
+    updatePurchaseState(ingredients) {
+
+        const sum = Object.keys(ingredients)
+        .map(igKey => {
+            return ingredients[igKey]
+        })
+        .reduce((sum, el) => {
+            return sum + el
+        }, 0)
+        this.setState({purchasable: sum > 0})
     }
 
     addIngredientHandler = (type) => {
@@ -36,6 +49,7 @@ class BurgerBuilder extends Component {
         this.setState({
             totalPrice: newPrice, ingredients: updatedIngredients
         });
+        this.updatePurchaseState(updatedIngredients)
     }
 
     removeIngredientHandler = (type) => {
@@ -54,6 +68,7 @@ class BurgerBuilder extends Component {
         this.setState({
             totalPrice: newPrice, ingredients: updatedIngredients
         });
+        this.updatePurchaseState(updatedIngredients)
     }
 
     render(){
@@ -71,6 +86,7 @@ class BurgerBuilder extends Component {
                  ingredientAdded={this.addIngredientHandler}
                  ingredientRemove={this.removeIngredientHandler}
                  disabled={disabledInfo}
+                 purchasable={this.state.purchasable}
                  price={this.state.totalPrice}
                  />
             </Auxilliary>
